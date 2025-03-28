e4d9b27b7ed8bfc0870512e4e33dd50f4a3299a1
yadavrobin <yadavrobin405@gmail.com>
2025-03-28 19:07:57 +0530

    created document schema and apis for documents

--- routes/documentRoutes.js
+++ routes/documentRoutes.js
@@ -55,12 +55,18 @@ router.get('/:userId', async (req, res) => {
             return res.status(404).json({ message: "No documents found for this user" });
         }
 
-        res.status(200).json(documents);
+        // Extract only Cloudinary URLs
+        const documentUrls = documents.map(doc => ({
+            documentUrl: doc.cloudinaryUrl
+        }));
+
+        res.status(200).json(documentUrls);
     } catch (error) {
         console.error("Fetch Error:", error);
         res.status(500).json({ message: "Server error", error });
     }
 });
+
 
 
 module.exports = router;
